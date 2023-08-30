aa8f0050e2a8f5d8637081e778b859c3bfd19329
Hanneszz <118542657+Hanneszz@users.noreply.github.com>
2023-08-30 10:11:48 +0200

    up

--- main.m
+++ main.m
@@ -3,36 +3,36 @@ close all
 
 %% Intruder geometry
 folder = 'cylinder';                               % cylinder, simple, robottip
-object = 'cylinder';                                  % name of stl
+object = 'cylinder2';                                  % name of stl
 triangle_size_calculation = 'normal';            % 'Fine', 'Normal', 'Rough', 'VeryRough'
-triangle_size_visualization = 'rough';          % 'Fine', 'Normal', 'Rough', 'VeryRough'
+triangle_size_visualization = 'normal';          % 'Fine', 'Normal', 'Rough', 'VeryRough'
 rotation_angle = 0;                             % rotate intruder around x-axis
-colors_diverging = brewermap([], 'RdBu');
-colors_sequential = brewermap([], 'Oranges');
+colors_diverging = 'jet'; % brewermap([], 'RdBu')
+colors_sequential = 'jet'; % brewermap([], 'Oranges')
 
 
 %% Physical Properties
-rho_c = 1310;                                   % bulk density of the sand in kg/m³   
-mu_int = 0.21;                                  % internal friction coefficient of the sand
-mu_surf = 0.4;                               % intruder-surface interaction coefficient
+rho_c = 1520;                                   % bulk density of the sand in kg/m³   
+mu_int = tan(deg2rad(20));                                  % internal friction coefficient of the sand
+mu_surf = 0.30;                               % intruder-surface interaction coefficient
 gravity =  9.81;                                 % gravity in m/s²
-xi_n = 0.12 * 10^6; % initially in N/m³ rho_c * gravity * (894*mu_int^3 - 386*mu_int^2 + 89*mu_int)
+xi_n = rho_c * gravity * (894*mu_int^3 - 386*mu_int^2 + 89*mu_int); % initially in N/m³ rho_c * gravity * (894*mu_int^3 - 386*mu_int^2 + 89*mu_int)
 
 
 %% Movement parameters
-rotation = true;                                % true or false
+rotation = 0;                                % true or false
 linear_velocity = 0.1;                        % linear velocity in m/s
 direction_angle_xz = -90 * pi / 180;            % angle between direction and x-z-axis
 direction_angle_y = -90 * pi / 180;             % angle between direction and y-axis
-angular_velocity = [0, 0, -1*pi];               % angular velocity in rad/s
+angular_velocity = [0, 0, -2*pi];               % angular velocity in rad/s
 direction_vector = [round(cos(direction_angle_xz), 15) ...
     round(cos(direction_angle_y), 15) round(sin(direction_angle_xz), 15)];
 
 
 %% Depth parameters
 start_depth = 0.00;
-end_depth = 0.125;
-step_size = 0.125;
+end_depth = 0.13;
+step_size = 0.01;
 
 
 %% Plot selection
@@ -43,9 +43,9 @@ show_f_quiver = 0;
 show_alpha = 0;
 
 show_f_scatter = 0;
-show_f_scatterxyz = 1;
+show_f_scatterxyz = 0;
 
-show_results = 0;
+show_results = 1;
 
 saveFigures = 0;
 
--- plots/plotFScatterxyz.m
+++ plots/plotFScatterxyz.m
@@ -9,31 +9,38 @@ max_pressure = max(max(abs(pressures)));
 % Create a tiled layout with 1 row and 3 columns
 tl = tiledlayout(1,3,'TileSpacing','loose','Padding','compact');
 
+size_data1 = 1000 * max_pressure / max(abs(pressures(:,1))) * abs(pressures(:,1));
+size_data1 = max(size_data1, eps); % eps is MATLAB's smallest positive value
+size_data2 = 1000 * max_pressure / max(abs(pressures(:,2))) * abs(pressures(:,2));
+size_data2 = max(size_data2, eps); 
+size_data3 = 1000 * max_pressure / max(abs(pressures(:,3))) * abs(pressures(:,3));
+size_data3 = max(size_data3, eps); 
+
 % Plot forces on each point of the mesh (scatter x)
 nexttile(tl);
 hold on;
 trimesh(TRG_visual, 'LineWidth', 0.1, 'EdgeColor', '#888888', 'FaceAlpha', 0);
-s1 = scatter3(points_include(:,1), points_include(:,2), points_include(:,3), 'filled', 'MarkerEdgeColor', 'none', 'CData', abs(pressures(:,1)), 'SizeData', 1000*max_pressure/max(abs(pressures(:,1)))*abs(pressures(:,1)));
+s1 = scatter3(points_include(:,1), points_include(:,2), points_include(:,3), 'filled', 'MarkerEdgeColor', 'none', 'CData', pressures(:,1), 'SizeData', size_data1);
 colormap(colors_sequential);
-clim([0 max_pressure]);
+clim([-max_pressure max_pressure]);
 setPlotProperties(x_range, y_range, z_range);
 
 % Plot forces on each point of the mesh (scatter y)
 nexttile(tl);
 hold on;
 trimesh(TRG_visual, 'LineWidth', 0.1, 'EdgeColor', '#888888', 'FaceAlpha', 0);
-s2 = scatter3(points_include(:,1), points_include(:,2), points_include(:,3), 'filled', 'MarkerEdgeColor', 'none', 'CData', abs(pressures(:,2)), 'SizeData', 1000*max_pressure/max(abs(pressures(:,2)))*abs(pressures(:,2)));
+s2 = scatter3(points_include(:,1), points_include(:,2), points_include(:,3), 'filled', 'MarkerEdgeColor', 'none', 'CData', pressures(:,2), 'SizeData', size_data2);
 colormap(colors_sequential);
-clim([0 max_pressure]);
+clim([-max_pressure max_pressure]);
 setPlotProperties(x_range, y_range, z_range);
 
 % Plot forces on each point of the mesh (scatter z)
 nexttile(tl);
 hold on;
 trimesh(TRG_visual, 'LineWidth', 0.1, 'EdgeColor', '#999999', 'FaceAlpha', 0);
-s3 = scatter3(points_include(:,1), points_include(:,2), points_include(:,3), 'filled', 'MarkerEdgeColor', 'none', 'CData', abs(pressures(:,3)), 'SizeData', 1000*max_pressure/max(abs(pressures(:,3)))*abs(pressures(:,3)));
+s3 = scatter3(points_include(:,1), points_include(:,2), points_include(:,3), 'filled', 'MarkerEdgeColor', 'none', 'CData', pressures(:,3), 'SizeData', size_data3);
 colormap(colors_sequential);
-clim([0 max_pressure]);
+clim([-max_pressure max_pressure]);
 setPlotProperties(x_range, y_range, z_range);
 
 % Add colorbar to the last subplot
